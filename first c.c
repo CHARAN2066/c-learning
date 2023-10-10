b5aeb1839133edbb21d19a8bc5616443f6ae4b85
CHARAN2066 <147472977+CHARAN2066@users.noreply.github.com>
2023-10-10 11:23:43 +0530

    Add files via upload

---
+++ first c.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	Printf("Enter two numbers to find the max");
+	scanf("%d %d",&a,&b);
+	if(a>b)
+	printf("%d is the max",a);
+	else
+	printf("%d is max",b);
+}
